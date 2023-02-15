2689adb6742e2200b1be6d43bb9d1a9164db4fb8
Keernom <keernom.gdev@gmail.com>
2023-02-15 16:47:18 +0300

    CLI character controller repo setup.

---
+++ CommandManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+public class CommandManager : MonoBehaviour
+{
+    [SerializeField] private PlayerController _playerController;
+    [SerializeField] private TMP_Text _commandBlock;
+
+    private Dictionary<string, Action<string>> _commands = new Dictionary<string, Action<string>>();
+    private Queue<string[]> _commandExecutionOrder = new Queue<string[]>();
+
+    private void Awake()
+    {
+        _commands.Add("Jump", (settings) => _playerController.Jump(settings));
+        _commands.Add("Run", (settings) => _playerController.Run(settings));
+        _commands.Add("Wait", (settings) => _playerController.Wait(settings));
+    }
+
+    public async void CompileCode()
+    {
+        string[] dividedText = _commandBlock.text.Split(';');
+
+        foreach(var e in dividedText)
+        {
+            string element = e.Replace("\n", "");
+            string[] commandText = element.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            if (_commands.ContainsKey(commandText[0]))
+                _commandExecutionOrder.Enqueue(commandText);
+        }
+
+        while (_commandExecutionOrder.Count > 0)
+        {
+            string[] command = _commandExecutionOrder.Dequeue();
+            if (command[0] == "Wait")
+                await Task.Run(() => _commands[command[0]].Invoke(command[1]));
+            else
+                _commands[command[0]].Invoke(command[1]);
+        }
+    }    
+}
---
+++ PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+    private Dictionary<string, Vector2> _directions = new Dictionary<string, Vector2>();
+    private Vector2 _startPosition;
+
+    private void Awake()
+    {
+        _directions.Add("left", Vector2.left);
+        _directions.Add("right", Vector2.right);
+    }
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _startPosition = transform.position;
+    }
+
+    public void Jump(params string[] settings)
+    {
+        if (settings[0].Contains(','))
+            settings[0] = settings[0].Replace(',', '.');
+
+        float jumpForce = float.Parse(settings[0], CultureInfo.InvariantCulture);
+        _rigidbody2D.AddForce(Vector2.up * 200 * jumpForce);
+    }
+
+    public void Run(params string[] settings)
+    {
+        _rigidbody2D.AddForce(_directions[settings[0]] * 150);
+    }
+
+    public void Wait(string time)
+    {
+        if (time.Contains(','))
+            time = time.Replace(',', '.');
+
+        float parsedTime = float.Parse(time, CultureInfo.InvariantCulture);
+        int milliseconds = (int)(parsedTime * 1000);
+        Thread.Sleep(milliseconds);
+    }
+
+    public void ResetPosition()
+    {
+        transform.position = _startPosition;
+    }
+}
